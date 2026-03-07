b460eb8743275548e00b82674a3eed7930bebea3
Kumaran <kumaran@anatechin.com>
2026-03-07 21:27:05 +0530

    .

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-router.use('/auth', require('./auth.routes'));
-router.use('/products', require('./product.routes'));
-router.use('/cart', require('./cart.routes'));
-router.use('/orders', require('./order.routes'));
-router.use('/admin', require('./admin.routes'));
-
-module.exports = router;
